08d8dd486eeaa6ef80e6cd18b3e6f7130b0914e2
Rafael Damasceno <rafaeldamascenowhy@gmail.com>
2024-06-19 18:37:57 -0300

    :sparkles: feat: primeiro projeto

--- src/components/ProjectBox/ProjectBox.tsx
+++ src/components/ProjectBox/ProjectBox.tsx
@@ -3,7 +3,9 @@ import StyledButton from "../StyledButton/StyledButton";
 import { motion } from "framer-motion";
 
 const StyledImg = styled("img")(() => ({
-  height: "100%",
+  width: "80%",
+  maxWidth: "200px",
+  objectFit: "contain",
 }));
 
 const containerVariants = {
@@ -18,37 +20,56 @@ const itemVariants = {
   visible: { opacity: 1 },
 };
 
-const ProjectBox = ({ title, dateRange, imageUrl, description, technologies, viewProjectUrl, viewCodeUrl }: { 
-  title: string; 
-  dateRange: string; 
-  imageUrl: string; 
-  description: string; 
-  technologies: string; 
-  viewProjectUrl: string; 
-  viewCodeUrl: string; 
+const ProjectBox = ({
+  title,
+  dateRange,
+  imageUrl,
+  description,
+  technologies,
+  viewProjectUrl,
+  viewCodeUrl,
+}: {
+  title: string;
+  dateRange: string;
+  imageUrl: string;
+  description: string;
+  technologies: string;
+  viewProjectUrl: string;
+  viewCodeUrl: string;
 }) => (
   <Grid item xs={12} md={5}>
     <motion.div variants={itemVariants}>
-      <Box sx={{ border: "1px solid grey", display: "flex", flexDirection: "column", height: "100%" }} p={2}>
-        <Typography color="#fff" variant="h6">{title}</Typography>
-        <Typography color="#fff" fontSize={12}>{dateRange}</Typography>
-        <Box textAlign="center">
-          <StyledImg src={imageUrl} alt={title} style={{ width: "80%", maxWidth: "300px" }} />
+      <Box
+        sx={{
+          border: "1px solid grey",
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+          minHeight: "400px",
+          maxHeight: "600px",
+          overflow: "hidden",
+        }}
+        p={2}
+      >
+        <Typography color="#fff" variant="h6" noWrap>{title}</Typography>
+        <Typography color="#fff" fontSize={12} noWrap>{dateRange}</Typography>
+        <Box textAlign="center" flex="1" display="flex" alignItems="center" justifyContent="center">
+          <StyledImg src={imageUrl} alt={title} />
         </Box>
-        <Typography color="#fff" pb={2} pl={2} fontSize={14} flex="1">
+        <Typography color="#fff" pb={2} pl={2} fontSize={14} style={{ overflow: "hidden", textOverflow: "ellipsis" }}>
           {description}
         </Typography>
-        <Typography color="#fff" pb={2} pl={2} fontSize={14}>
+        <Typography color="#fff" pb={2} pl={2} fontSize={14} noWrap>
           Technologies: {technologies}
         </Typography>
         <Grid container display="flex" justifyContent="center" spacing={3}>
           <Grid item xs={12} md={6} display="flex" justifyContent="center">
-            <StyledButton onClick={() => window.open(viewProjectUrl, '_blank')}>
+            <StyledButton onClick={() => window.open(viewProjectUrl, "_blank")}>
               <Typography fontSize={12}>View Project</Typography>
             </StyledButton>
           </Grid>
           <Grid item xs={12} md={6} display="flex" justifyContent="center">
-            <StyledButton onClick={() => window.open(viewCodeUrl, '_blank')}>
+            <StyledButton onClick={() => window.open(viewCodeUrl, "_blank")}>
               <Typography fontSize={12}>View Code</Typography>
             </StyledButton>
           </Grid>
@@ -61,23 +82,16 @@ const ProjectBox = ({ title, dateRange, imageUrl, description, technologies, vie
 const ProjectBoxes = () => {
   const projectData = [
     {
-      title: "Project Example 1",
-      dateRange: "jun 2023 - dec 2023",
-      imageUrl: "https://via.placeholder.com/150",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus vel voluptate enim quibusdam ab impedit dolorem, facere velit dicta id possimus quis, nesciunt distinctio veritatis dolorum. Minima explicabo assumenda similique.",
-      technologies: "JavaScript, Html, css",
-      viewProjectUrl: "#",
-      viewCodeUrl: "#",
+      title: "Bot linkedIn",
+      dateRange: "jun 2024 - jun 2024",
+      imageUrl: "https://raw.githubusercontent.com/Synergy4tech/Bot-LinkedIn/main/src/assets/preview.jpg",
+      description:
+        "Este é um bot desenvolvido em Python usando Selenium para automatizar o processo de conexões no LinkedIn. Ele é projetado para ajudar a expandir sua rede de contatos de maneira eficiente e sem esforço manual.",
+      technologies: "Python, Selenium, CustomTkinter",
+      viewProjectUrl: "https://github.com/Synergy4tech/Bot-LinkedIn/releases/tag/bot",
+      viewCodeUrl: "https://github.com/Synergy4tech/Bot-LinkedIn",
     },
-    {
-      title: "Project Example 2",
-      dateRange: "jun 2023 - dec 2023",
-      imageUrl: "https://via.placeholder.com/150",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus vel voluptate enim quibusdam ab impedit dolorem, facere velit dicta id possimus quis, nesciunt distinctio veritatis dolorum. Minima explicabo assumenda similique.",
-      technologies: "JavaScript, Html, css",
-      viewProjectUrl: "#",
-      viewCodeUrl: "#",
-    },
+    
   ];
 
   return (
